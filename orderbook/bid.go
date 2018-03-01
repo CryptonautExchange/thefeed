05daa67873175801bfa6c301b3ab82855e28477d
Your Name <you@example.com>
2018-03-01 19:01:10 -0300

    Add database objects for asks and bids for our simulated orderbook API

---
+++ orderbook/bid.go
@@ -0,0 +1,16 @@
+package orderbook
+
+import (
+	//"fmt"
+	"time"
+
+	"labix.org/v2/mgo/bson"
+)
+
+type Bid struct {
+	ID        bson.ObjectId `storm:"id"`
+	CreatedAt time.Time     `storm:"index"`
+	Currency  string        `storm:"index"`
+	Commodity string        `storm:"index"`
+	Price     string        `storm:"index"`
+}
