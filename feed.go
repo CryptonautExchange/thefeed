d726ce870e8aeae4cbf296ac22cb2d29b80513df
Your Name <you@example.com>
2018-03-02 02:57:39 -0300

    feed.go: Implement some enhanced UI features because doing this early and foten will develop nice UI habits that are easy to drop into future programs>

--- feed.go
+++ feed.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/asdine/storm"
 	. "github.com/hackwave/color"
+
+	"libs/ui/spinner"
 )
 
 func main() {
@@ -14,9 +16,16 @@ func main() {
 	fmt.Println(Magenta("The Feed"), Gray(":"), Blue("Market Data Mocking Library"))
 	fmt.Println(Gray("====================================="))
 	db, err := storm.Open("orderbook.db")
+	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
+	s.Start()
+	time.Sleep(4 * time.Second)
+	fmt.Println(Gray("[Database] Initializing orderbook database..."))
 	if err != nil {
 		fmt.Println(Red("Fatal Error:"), Gray("Failed to load orderbook database, exiting..."))
 		fmt.Println(Red("["), Gray(err), Red("]"))
+	} else {
+		s.Stop()
+		fmt.Println(Green("[Database] Successfully opened the orderbook database."))
 	}
 	defer db.Close()
 
